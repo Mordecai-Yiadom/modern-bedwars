bd2537158a4f8f3809ae1f0cf1a9c6db4e6dee82
Mordecai Yiadom <mordeyiadom@gmail.com>
2025-07-10 01:54:29 -0400

    Change ItemSpawner class from abstract to concrete

--- src/com/krousant/modernbedwars/game/ItemSpawner.java
+++ src/com/krousant/modernbedwars/game/ItemSpawner.java
@@ -11,7 +11,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.LinkedList;
 
-public abstract class ItemSpawner
+public class ItemSpawner
 {
 
     private final LinkedList<SpawnableItem> SPAWNABLE_ITEMS = new LinkedList<>();
